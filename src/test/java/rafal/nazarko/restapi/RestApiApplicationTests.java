862157f48d23daffe962594cbb8c8a2e11847523
Rafał Nazarko <rafal.nazarko@gmail.com>
2022-05-15 21:28:18 +0200

    first commit

---
+++ src/test/java/rafal/nazarko/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rafal.nazarko.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
